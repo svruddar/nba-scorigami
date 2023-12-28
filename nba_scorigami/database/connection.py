b1018a7d6ea6fc1464a3e12655674740dc15da05
svruddar <svrdev26@gmail.com>
2023-12-28 19:01:01 +0530

    MySQL database integration

---
+++ nba_scorigami/database/connection.py
@@ -0,0 +1,48 @@
+import configparser
+import os
+import mysql.connector
+from mysql.connector import Error
+
+CONFIG_FILE = "db.config"
+
+# Getting config information
+def read_config():
+    curr_path = os.path.abspath(__file__)
+    config_path = os.path.join(os.path.dirname(curr_path), CONFIG_FILE)
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config["Credentials"]
+
+
+# Creating database connection
+def create_db_connection(host_name, user_name, user_password, db):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name, user=user_name, passwd=user_password, database=db
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+"""
+CREATE TABLE score_pairs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pair VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE games (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    score_pair_id INT,
+    date DATE,
+    home_team VARCHAR(255),
+    home_score INT,
+    away_team VARCHAR(255),
+    away_score INT,
+    FOREIGN KEY (score_pair_id) REFERENCES score_pairs(id) ON DELETE CASCADE,
+    UNIQUE KEY identifier (date, home_team, away_team)
+);
+"""
--- nba_scorigami/database/scrape.py
+++ nba_scorigami/database/scrape.py
@@ -1,19 +1,19 @@
-import collections
+import time
+from datetime import datetime
 from urllib.request import urlopen
 
 import pandas as pd
 from bs4 import BeautifulSoup
-
+from connection import create_db_connection, read_config
+from mysql.connector import Error
 
 BBAL_REF_BASE = "https://www.basketball-reference.com"
-SEASONS_URL = BBAL_REF_BASE + "/leagues/"
+config = read_config()
 
-score_data = collections.defaultdict(list)
-
-
+# Building a list of urls for each season's data
 def get_season_pages() -> list[str]:
     urls = []
-    html = urlopen(SEASONS_URL).read().decode("utf-8")
+    html = urlopen(BBAL_REF_BASE + "/leagues/").read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
     table = soup.find("table", class_="suppress_all sortable stats_table")
     for row in table.find_all("tr"):
@@ -23,8 +23,8 @@ def get_season_pages() -> list[str]:
             urls.append(BBAL_REF_BASE + season_path)
     return urls
 
-
-def get_season_month_pages(season_url):
+# Building a list of urls for each month's data in a season
+def get_month_pages(season_url):
     urls = []
     html = urlopen(season_url).read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
@@ -35,7 +35,8 @@ def get_season_month_pages(season_url):
     return urls
 
 
-def store_month_scores(month_url):
+# Storing each month's games in the database
+def store_month_scores(month_url, conn):
     scores_df = pd.read_html(
         month_url,
         attrs={"class": "suppress_glossary sortable stats_table"},
@@ -43,23 +44,57 @@ def store_month_scores(month_url):
     )[0]
     for i in range(len(scores_df)):
         row = scores_df.loc[i]
-        if not (pd.isnull(row["PTS"]) or row["PTS"] == "Playoffs"):
+        if row["PTS"] == "Playoffs":
+            print(f"ROW: {row}")
+        if not (
+            pd.isnull(row["PTS"]) or pd.isnull(row["PTS.1"]) or row["PTS"] == "Playoffs"
+        ):
             home_score, away_score = int(row["PTS"]), int(row["PTS.1"])
-            key = f"{min(home_score, away_score)}-{max(home_score, away_score)}"
-            value = {
-                "date": row["Date"],
-                "home team": row["Home/Neutral"],
-                "home score": home_score,
-                "away": row["Visitor/Neutral"],
-                "away score": away_score,
-            }
-            score_data[key].append(value)
+            key = f"{max(home_score, away_score)}:{min(home_score, away_score)}"
+
+            cursor = conn.cursor()
+
+            # Store key in score_pairs table
+            try:
+                cursor.execute("INSERT INTO score_pairs (pair) VALUES (%s)", (key,))
+            except Error as err:
+                print(err)
+
+            # Get primary key of the pair from score_pairs
+            try:
+                cursor.execute("SELECT id FROM score_pairs WHERE pair = %s", (key,))
+                score_pair_id = cursor.fetchone()[0]
+            except Error as err:
+                print(err)
+
+            # Store game data in games table
+            try:
+                formatted_date = datetime.strptime(row["Date"], "%a, %b %d, %Y").date()
+                cursor.execute(
+                    "INSERT INTO games (score_pair_id, date, home_team, home_score, away_team, away_score) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (
+                        score_pair_id,
+                        formatted_date,
+                        row["Home/Neutral"],
+                        home_score,
+                        row["Visitor/Neutral"],
+                        away_score,
+                    ),
+                )
+            except Error as err:
+                print(err)
+
+            conn.commit()
 
 
+# Building the database
 def main():
-    seasons = get_season_pages()
-    for s in seasons:
-        print(s)
+    conn = create_db_connection("localhost", "root", config.get("PASS"), "scores")
+    for season in get_season_pages():
+        for month in get_month_pages(season):
+            store_month_scores(month, conn)
+            time.sleep(2)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
